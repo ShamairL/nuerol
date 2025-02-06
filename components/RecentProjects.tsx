add4c27bb3b23bfce75b09c34d1730aaa46d3724
Shamair <147728463+ShamairL@users.noreply.github.com>
2025-02-06 02:54:24 -0500

    y

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -10,7 +10,7 @@ const RecentProjects = () => {
     <div className="py-20">
       <h1 className="heading">
         A glimpse of the many projects {" "}
-        <span className="text-purple">You'll build.</span>
+        <span className="text-purple">You`&apos;`ll build.</span>
       </h1>
       <div className="flex flex-wrap items-center justify-center p-4 gap-16 mt-10">
         {projects.map((item) => (
